1e7786c9d5817f28ff62c7451c9d71ba94e46696
Hagar Mohamed <hagar.mohamed8003@gmail.com>
2026-07-31 05:29:23 +0300

    Fix

--- be/src/schemas/portfolio.js
+++ be/src/schemas/portfolio.js
@@ -2,6 +2,6 @@ import joi from "joi";
 import { estimateSingleSchema } from "./house.js";
 
 export const estimatePortfolioSchema = joi.object({
-  records: joi.array().min(1)..max(100).required(),
+  records: joi.array().min(1).max(100).required(),
 });
 
