614198701f9f2ffebd385776bd4a5e4cc9f15c9e
Ahmed Mohamed elmitwalli Sakr <Ahmadsakr200@gmail.com>
2026-07-31 03:19:04 +0300

    Remove Portfolio route from App component

--- Website/src/App.jsx
+++ Website/src/App.jsx
@@ -4,7 +4,6 @@ import Layout from "./Components/layout";
 
 import Home from "./pages/Home";
 import Estimate from "./pages/Estimate";
-import Portfolio from "./pages/Portfolio";
 import About from "./pages/About";
 
 export default function App() {
@@ -13,7 +12,6 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/estimate" element={<Estimate />} />
-        <Route path="/portfolio" element={<Portfolio />} />
         <Route path="/about" element={<About />} />
       </Routes>
     </Layout>
