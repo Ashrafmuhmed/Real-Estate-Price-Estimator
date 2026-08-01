0b199eb1c7e9e033220f397ac1e0b381383f5394
Ashraf Muhammed <bakr29420@gmail.com>
2026-08-01 22:26:03 +0300

    fix(validation): min bathrooms fixed to be 1

--- backend/src/schemas/house.js
+++ backend/src/schemas/house.js
@@ -3,6 +3,6 @@ import Joi from "joi";
 export const estimateSingleSchema = Joi.object({
   sqft: Joi.number().positive().min(300).max(10000).required(),
   bedrooms: Joi.number().integer().positive().min(1).max(10).required(),
-  bathrooms: Joi.number().positive().min(0.5).max(10).required(),
+  bathrooms: Joi.number().positive().min(1).max(10).required(),
   age: Joi.number().integer().min(0).max(150).required(),
 });
